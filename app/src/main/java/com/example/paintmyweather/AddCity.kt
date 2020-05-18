6fc785dfd1f236a67ae985b3d7857e1bb2b977c0
Hariharasudhan177 <hariharasudhan.gunasekaran@st.ovgu.de>
2020-05-18 14:43:42 +0530

    Acitivity and Layout set up

---
+++ app/src/main/java/com/example/paintmyweather/AddCity.kt
@@ -0,0 +1,12 @@
+package com.example.paintmyweather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddCity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_city)
+    }
+}
--- app/src/main/java/com/example/paintmyweather/MainActivity.kt
+++ app/src/main/java/com/example/paintmyweather/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.paintmyweather
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fab: View = findViewById(R.id.fab)
+
+        fab.setOnClickListener { view ->
+            val intent = Intent(this, AddCity::class.java)
+            startActivity(intent)
+        }
+
     }
+
 }
